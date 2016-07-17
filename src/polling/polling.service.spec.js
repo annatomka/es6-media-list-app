bf9355f9fcf3474d64d22ac043bf2a879e4e40a3
Tomka Anna <tomkaanna@gmail.com>
2016-07-17 21:37:04 +0200

    Fix polling service spec event emitter emit test

--- src/polling/polling.service.spec.js
+++ src/polling/polling.service.spec.js
@@ -1,112 +1,118 @@
 import { PollingService } from './polling.service';
-import { EVENT_POLLING_INTERVAL_CHANGED } from '../app.constants';
+import { EVENT_POLLING_INTERVAL_CHANGED, EVENT_POLLING_RESULT } from '../app.constants';
 
 describe('PollingService', () => {
     let eventEmitterSpy, pollingService, apiServiceSpy;
-    const EVENT_KEY = "dummy event key";
 
     beforeEach(()=> {
         eventEmitterSpy = jasmine.createSpyObj("EventEmitter", ['on', 'emit']);
         apiServiceSpy = jasmine.createSpyObj("ApiService", ['getAllMediaItems']);
-
     });
 
     describe('constructor', ()=> {
+
         it('should register restartWithNewIntervalSeconds on EVENT_POLLING_INTERVAL_CHANGED', () => {
             pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
-           expect(eventEmitterSpy.on).toHaveBeenCalledWith(EVENT_POLLING_INTERVAL_CHANGED, pollingService.restartWithNewIntervalSeconds);
+            expect(eventEmitterSpy.on).toHaveBeenCalledWith(EVENT_POLLING_INTERVAL_CHANGED, pollingService.restartWithNewIntervalSeconds);
         });
     });
 
-    describe('setPollingIntervalMilliseconds', ()=>{
-        it('should exist', ()=>{
-           expect(pollingService.setPollingIntervalMilliseconds).toBeDefined();
+    describe('setPollingIntervalMilliseconds', ()=> {
+        beforeEach(()=> {
+            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
         });
 
-       it('should set polling interval ms', ()=>{
-           pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
-           var newPollingInterval = 120000;
-           pollingService.setPollingIntervalMilliseconds(newPollingInterval);
-           expect(pollingService.pollingIntervalMilliseconds).toEqual(newPollingInterval);
-       })
+        it('should exist', ()=> {
+            expect(pollingService.setPollingIntervalMilliseconds).toBeDefined();
+        });
+
+        it('should set polling interval ms', ()=> {
+            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
+            var newPollingInterval = 120000;
+            pollingService.setPollingIntervalMilliseconds(newPollingInterval);
+            expect(pollingService.pollingIntervalMilliseconds).toEqual(newPollingInterval);
+        })
     });
 
-    describe('start', ()=>{
-        beforeEach(()=>{
+    describe('start', ()=> {
+        beforeEach(()=> {
+            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
             jasmine.clock().install();
         });
 
-        afterEach(()=>{
+        afterEach(()=> {
             jasmine.clock().uninstall();
         });
 
-        it('should exist', ()=>{
-            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
+        it('should exist', ()=> {
             expect(pollingService.start).toBeDefined();
-        })
+        });
 
-        it('should call poll immediately', ()=>{
-            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
+        it('should call poll immediately', ()=> {
             spyOn(pollingService, 'poll');
             pollingService.start();
             expect(pollingService.poll).toHaveBeenCalled();
             expect(pollingService.poll.calls.count()).toEqual(1);
         });
 
-        it('should call poll after the given interval',()=>{
-            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
+        it('should call poll after the given interval', ()=> {
             spyOn(pollingService, 'poll');
             pollingService.start();
             expect(pollingService.poll.calls.count()).toEqual(1);
             jasmine.clock().tick(pollingService.pollingIntervalMilliseconds);
-
             expect(pollingService.poll.calls.count()).toEqual(2);
         });
     });
 
-    describe('poll', ()=>{
-        beforeEach(()=>{
-                jasmine.clock().install();
-
-            apiServiceSpy.getAllMediaItems.and.callFake(()=>{
-                return $.Deferred().promise();
-            });
+    describe('poll', ()=> {
+        let getAllMediaItemsDeferred;
+        beforeEach(()=> {
+            getAllMediaItemsDeferred = $.Deferred();
+            jasmine.clock().install();
+            apiServiceSpy.getAllMediaItems.and.returnValue(getAllMediaItemsDeferred);
             pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
         });
 
-        afterEach(()=>{
+        afterEach(()=> {
             jasmine.clock().uninstall();
         });
 
-        it('should call api service getAllMediaItems', ()=>{
-            pollingService.poll();
+        it('should exist', ()=> {
+            expect(pollingService.poll).toBeDefined();
+        });
 
+        it('should call api service getAllMediaItems', ()=> {
+            pollingService.poll();
             expect(apiServiceSpy.getAllMediaItems).toHaveBeenCalled();
         });
 
-        it('should call EventEmitter emit', ()=>{
+        it('should call EventEmitter emit', ()=> {
+            let getAllMediaItemsResult = "dummy values";
             pollingService.poll();
-            jasmine.clock().tick(100);
-
-            expect(eventEmitterSpy.emit).toHaveBeenCalled();
-
-
+            getAllMediaItemsDeferred.resolve(getAllMediaItemsResult);
+            jasmine.clock().tick(0);
+            expect(eventEmitterSpy.emit).toHaveBeenCalledWith(EVENT_POLLING_RESULT,getAllMediaItemsResult);
         });
     });
 
-    describe('stop', ()=>{
-        beforeEach(()=>{
-            apiServiceSpy.getAllMediaItems.and.callFake(()=>{
-                return $.Deferred().promise();
-            });
+    describe('stop', ()=> {
+        let getAllMediaItemsDeferred;
+
+        beforeEach(()=> {
+            getAllMediaItemsDeferred = $.Deferred();
+            apiServiceSpy.getAllMediaItems.and.returnValue(getAllMediaItemsDeferred);
             pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
         });
 
-        it('should exist', ()=>{
+        beforeEach(()=> {
+            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
+        });
+
+        it('should exist', ()=> {
             expect(pollingService.stop).toBeDefined();
         });
 
-        it('should not call clearInterval when there is no polling interval id',() => {
+        it('should not call clearInterval when there is no polling interval id', () => {
             spyOn(window, 'clearInterval');
 
             expect(pollingService.pollingIntervalId).toBeNull();
@@ -114,7 +120,7 @@ describe('PollingService', () => {
             expect(window.clearInterval).not.toHaveBeenCalledWith(pollingService.pollingIntervalId);
         });
 
-        it('should call clearInterval when there was polling interval id', ()=>{
+        it('should call clearInterval when there was polling interval id', ()=> {
             spyOn(window, 'clearInterval');
 
             pollingService.start();
@@ -126,12 +132,19 @@ describe('PollingService', () => {
         });
     });
 
-    describe('restart', ()=>{
-        it('should exist', ()=>{
+    describe('restart', ()=> {
+        let getAllMediaItemsDeferred;
+        beforeEach(()=> {
+            getAllMediaItemsDeferred = $.Deferred();
+            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
+            apiServiceSpy.getAllMediaItems.and.returnValue(getAllMediaItemsDeferred);
+        });
+
+        it('should exist', ()=> {
             expect(pollingService.restart).toBeDefined();
         });
 
-        it('should call stop and start', ()=>{
+        it('should call stop and start', ()=> {
             spyOn(pollingService, 'start');
             spyOn(pollingService, 'stop');
             pollingService.restart();
@@ -140,16 +153,24 @@ describe('PollingService', () => {
         });
     });
 
-    describe('restartWithNewIntervalSeconds', ()=>{
-        it('should exist', ()=>{
-           expect(pollingService.restartWithNewIntervalSeconds).toBeDefined();
+    describe('restartWithNewIntervalSeconds', ()=> {
+        let getAllMediaItemsDeferred;
+        beforeEach(()=> {
+            getAllMediaItemsDeferred = $.Deferred();
+            apiServiceSpy.getAllMediaItems.and.returnValue(getAllMediaItemsDeferred);
+            pollingService = new PollingService(eventEmitterSpy, apiServiceSpy);
+
         });
 
-        it('should call setPollingIntervalMilliseconds', ()=>{
+        it('should exist', ()=> {
+            expect(pollingService.restartWithNewIntervalSeconds).toBeDefined();
+        });
+
+        it('should call setPollingIntervalMilliseconds', ()=> {
             let seconds = 40;
             spyOn(pollingService, 'setPollingIntervalMilliseconds');
             pollingService.restartWithNewIntervalSeconds(seconds);
-            expect(pollingService.setPollingIntervalMilliseconds).toHaveBeenCalledWith(seconds*1000);
+            expect(pollingService.setPollingIntervalMilliseconds).toHaveBeenCalledWith(seconds * 1000);
         })
     });
 });
