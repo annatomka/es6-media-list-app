06a45f2c962b14f1d8a56e1a1df6aaf8f43a3ca4
Tomka Anna <tomkaanna@gmail.com>
2016-07-19 10:10:30 +0200

    Add home component spec

--- src/home/home.component.spec.js
+++ src/home/home.component.spec.js
@@ -1,9 +1,34 @@
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', ()=>{
+describe('HomeComponent', ()=> {
     "use strict";
+    let eventEmitterSpy;
 
-    describe('activate', ()=>{
-        //TODO:
+    beforeEach(()=> {
+        eventEmitterSpy = jasmine.createSpyObj("EventEmitter", ['on', 'emit']);
+    });
+
+    describe('activate', ()=> {
+        let homeComponent;
+
+        beforeEach(()=> {
+            homeComponent = new HomeComponent(eventEmitterSpy)
+        });
+
+        it('should call super activate with #home element', ()=>{
+            //TODO: how to test super call??
+        });
+
+        it('should call allMediaListComponent activate', ()=>{
+            spyOn(homeComponent.allMediaListComponent, 'activate');
+            homeComponent.activate();
+            expect(homeComponent.allMediaListComponent.activate).toHaveBeenCalled();
+        });
+
+        it('should call watchLaterListComponent activate', ()=>{
+            spyOn(homeComponent.watchLaterListComponent, 'activate');
+            homeComponent.activate();
+            expect(homeComponent.watchLaterListComponent.activate).toHaveBeenCalled();
+        });
     });
 });
