33d2081695c1d5cab44136a1ef3ff0c30c360f54
Tomka Anna <tomkaanna@gmail.com>
2016-07-20 20:09:45 +0200

    Fix media item template

--- src/media-list/media.item.template.js
+++ src/media-list/media.item.template.js
@@ -7,7 +7,7 @@ export function mediaItemTemplate(mediaItem) {
             <div class="media-picture-container">
                 <img src="${mediaItem.picture}"/>
             </div>
-            <h4>${mediaItem.title} ${liveBadge()}</h4>
+            <h4>${mediaItem.title} ${liveBadge(mediaItem)}</h4>
             <div class="media-detail">
                 <span class="location">from ${mediaItem.location && mediaItem.location.city}, ${mediaItem.location && mediaItem.location.country}</span>
                 <span class="separator">•</span>
