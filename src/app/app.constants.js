b429fee7bcdd2b967bd20a1732d042623bf95ebb
Tomka Anna <tomkaanna@gmail.com>
2016-07-24 19:25:37 +0200

    Fix polling stopped message typo

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -6,5 +6,5 @@ export const EVENT_MEDIA_LIST_UPDATED = 'medialist:updated';
 export const API_URL = 'http://146.185.158.18/fake_api.php';
 export const POLLING_INTERVAL_MS = 10000;
 export const STORAGE_ID_WATCH_LATER = 'watch_later_items';
-export const POLLING_STOPPED_MESSAGE = '`There was an error in the request, ' +
+export const POLLING_STOPPED_MESSAGE = 'There was an error in the request, ' +
                                         'polling is stopped. Refresh the page.';
